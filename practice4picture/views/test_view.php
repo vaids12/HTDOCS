947994fde94e3aca9bc8397b2b5a7fea9c98ab45
vaids12 <vaidasvaupsas@gmail.com>
2022-01-28 18:08:06 +0200

    update

--- practice4picture/views/test_view.php
+++ practice4picture/views/test_view.php
@@ -7,9 +7,9 @@ include '../layout/header.php';
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card bg-light mb-8">
-               <p style= "color: <?php echo $color1?>"><?php echo $answer1." . Ilgiausia Lietuvos upė : Nemunas <br>";?></p> 
-               <p style= "color: <?php echo $color2?>"><?php echo $answer2." . Gilausias Lietuvos ežeras : Tauragnas <br>";?></p> 
-               <p style= "color: <?php echo $color3?>"><?php echo $answer3." . Didžiausias Lietuvos ežeras : Drūkšiai <br>";?></p> 
+               <p style= "color: <?php echo $color1?>; margin:10px"><?php echo $answer1." . Ilgiausia Lietuvos upė : Nemunas <br>";?></p> 
+               <p style= "color: <?php echo $color2?>; margin:10px "><?php echo $answer2." . Gilausias Lietuvos ežeras : Tauragnas <br>";?></p> 
+               <p style= "color: <?php echo $color3?> ; margin:10px"><?php echo $answer3." . Didžiausias Lietuvos ežeras : Drūkšiai <br>";?></p> 
                
 
          </div>
