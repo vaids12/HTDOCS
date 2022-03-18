bc7ab1010b20168af87ad947e94220c56a0ce6d2
vaids12 <vaidasvaupsas@gmail.com>
2022-03-18 20:31:41 +0200

    update

--- miniForum/db_conection.php
+++ miniForum/db_conection.php
@@ -8,7 +8,7 @@ $database="mini_forum";
 try {
     $conn= new PDO("mysql:host=$servername;dbname=$database",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    // echo"Connected to DB";
+  
 }catch(PDOException $e){
     echo"Connection failed: ".$e->getMassage();
 }
--- miniForum/views/chat.php
+++ miniForum/views/chat.php
@@ -38,29 +38,29 @@ $result=$query->fetchAll();
                        
                              foreach($result as $post){
 
-                                      $id=$post['id'];
-                                     $sql ="SELECT * FROM likes WHERE chatid='$id'";                                                                      
-                                      $query=$conn->prepare($sql);
-                                     $query->execute(); 
-                                    $likes=$query->fetchAll();
+                                        $id=$post['id'];
+                                        $sql ="SELECT * FROM likes WHERE chatid='$id'";                                                                      
+                                        $query=$conn->prepare($sql);
+                                        $query->execute(); 
+                                        $likes=$query->fetchAll();
                            
                                      if($_SESSION['username']===$post['nickname']){
-                                         echo "<div class='  p-3 mb-3 bg-light'> 
-                                         <span  style='color:blue;'>".$post['nickname']."</span>
-                                          <span style='font-size:9px;margin-left:10px;' >".$post['created']."</span>
-                                         <p>".$post['message']."</p>
-                                          <a class='btn btn-primary btn-sm'  href='../scripts/like.php?id=".$post['id']."'>LIKE&nbsp".$icon." &nbsp".count($likes)."</a>                                         
-                                          <a class='btn btn-warning btn-sm' style='margin-left:1000px;' href='chat_edit.php?id=".$post['id']."'>Edit</a>                                          
-                                          <a class='btn btn-danger btn-sm' style='margin-left:20px;' href='../views/chat_delete.php?id=".$post['id']."'>Delete</a>                                                                                                                      
-                                          </div>";
+                                            echo "<div class='  p-3 mb-3 bg-light'> 
+                                            <span  style='color:blue;'>".$post['nickname']."</span>
+                                            <span style='font-size:9px;margin-left:10px;' >".$post['created']."</span>
+                                            <p>".$post['message']."</p>
+                                            <a class='btn btn-primary btn-sm'  href='../scripts/like.php?id=".$post['id']."'>LIKE&nbsp".$icon." &nbsp".count($likes)."</a>                                         
+                                            <a class='btn btn-warning btn-sm' style='margin-left:1000px;' href='chat_edit.php?id=".$post['id']."'>Edit</a>                                          
+                                            <a class='btn btn-danger btn-sm' style='margin-left:20px;' href='../views/chat_delete.php?id=".$post['id']."'>Delete</a>                                                                                                                      
+                                            </div>";
                                      }else{
-                                         echo "<div class='  p-3 mb-3 bg-light'>                                  
-                                         <span  style='color:blue;'>".$post['nickname']."</span>
-                                         <span style='font-size:9px;margin-left:10px;' >".$post['created']."</span>
-                                         <p>".$post['message']."</p>
-                                         <a class='btn btn-primary btn-sm'  href='../scripts/like.php?id=".$post['id']."'>LIKE&nbsp".$icon." &nbsp".count($likes)."</a>
-                                                                   
-                                         </div>"; 
+                                            echo "<div class='  p-3 mb-3 bg-light'>                                  
+                                            <span  style='color:blue;'>".$post['nickname']."</span>
+                                            <span style='font-size:9px;margin-left:10px;' >".$post['created']."</span>
+                                            <p>".$post['message']."</p>
+                                            <a class='btn btn-primary btn-sm'  href='../scripts/like.php?id=".$post['id']."'>LIKE&nbsp".$icon." &nbsp".count($likes)."</a>
+                                                                    
+                                            </div>"; 
                                          }                                                                        
                              } 
                                   ?>                                                                                     
