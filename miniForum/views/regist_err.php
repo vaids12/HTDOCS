72e47f63bf5e8dceda46b47448e067e5a1232ec9
vaids12 <vaidasvaupsas@gmail.com>
2022-03-18 18:10:58 +0200

    uppdate

--- miniForum/views/regist_err.php
+++ miniForum/views/regist_err.php
@@ -1,11 +1,8 @@
 <?php 
-session_start();
-// if(!isset($_SESSION['username'])){
-//     header("Location: login.php");
-// }
+
 include '../layout/header.php';
 
-require_once("../db_conection.php");
+
 
 ?>
 <div class="container py-4">
